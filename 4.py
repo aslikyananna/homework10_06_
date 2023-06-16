0012162c5bb32ba422c32007990231e787bd20eb
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-16 18:54:24 +0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,6 +3,6 @@ def find_last_not_of(a, b):
         if i in b and i in a:
             index = a.index(i)
         return a[index-1]
-    print(find_last_not_of("hithere", "i,o,e,k"))
+    print(find_last_not_of("hithere", "i"))
 
 
