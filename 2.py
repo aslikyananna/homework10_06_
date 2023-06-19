f6a944bbb9278e2aec85e9e69227050d94ba14d2
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-19 16:14:28 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,12 @@
-def starts_with(a, b):
+def ends_with(a, b):
     k = len(a)-len(b)
+    if k < 0:
+        return False
     if a[k:] == b:
         return True
     else:
         return False
-print(starts_with("hithere", "there"))
+print(ends_with("hethere", "there"))
+
+
+
