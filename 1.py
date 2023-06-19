adb5015ee3f617140080ce8e8a411a15d8fc321e
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-19 16:05:35 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
 def starts_with(a, b):
     k = len(a)-len(b)
-    if a[k:] == b:
+    if len(a) < len(b):
+        return False
+    elif a[:(k-1)] == b:
         return True
     else:
         return False
-print(starts_with("hithere", "thekre"))
+print(starts_with("hi", "hit"))
+
